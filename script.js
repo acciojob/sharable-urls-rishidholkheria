ce69fc2687d93c9edc31e77857e918fc2d76ae7f
Rishi Dholkheria <57285705+rishidholkheria@users.noreply.github.com>
2025-05-10 23:51:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 // your code here
 
-const submitBtn = document.getElementById('submitBtn')
-submitBtn.addEventListener('click', () => {
-	event.preventDefault();
-	const name = document.getElementById("name").value.trim();
-    const year = document.getElementById("year").value.trim();
-	const baseUrl = "https://localhost:8080/";
-      const params = new URLSearchParams();
+const form = document.getElementById('myForm');
 
-      if (name !== "") {
-        params.append("name", name);
-      }
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent page reload
 
-      if (year !== "") {
-        params.append("year", year);
-      }
-      const fullUrl = params.toString() ? baseUrl + "?" + params.toString() : baseUrl;
-      document.getElementById("url").textContent = fullUrl;
-})
+  const name = document.getElementById("name").value.trim();
+  const year = document.getElementById("year").value.trim();
+  const baseUrl = "https://localhost:8080/";
+  const params = new URLSearchParams();
 
+  if (name !== "") {
+    params.append("name", name);
+  }
+  if (year !== "") {
+    params.append("year", year);
+  }
+
+  const fullUrl = params.toString() ? baseUrl + "?" + params.toString() : baseUrl;
+  document.getElementById("url").textContent = fullUrl;
+});
 
 
