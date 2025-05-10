43ad5ee54998cc7c19221346c7ddd14625a3dc84
Rishi Dholkheria <57285705+rishidholkheria@users.noreply.github.com>
2025-05-10 21:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+
+const submitBtn = document.getElementById('submitBtn')
+submitBtn.addEventListener('click', () => {
+	event.preventDefault();
+	const name = document.getElementById("name").value.trim();
+    const year = document.getElementById("year").value.trim();
+	const baseUrl = "https://localhost:8080/";
+      const params = new URLSearchParams();
+
+      if (name !== "") {
+        params.append("name", name);
+      }
+
+      if (year !== "") {
+        params.append("year", year);
+      }
+      const fullUrl = params.toString() ? baseUrl + "?" + params.toString() : baseUrl;
+      document.getElementById("url").textContent = fullUrl;
+})
+
+
+
